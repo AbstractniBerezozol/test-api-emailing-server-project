33651ced1413ec52374a4064157ac1c02466c9e2
AbstractniBerezozol <aleksandr.zolotarev@abstract.rs>
2024-08-16 17:24:02 +0200

    Test Email Sercice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
---
+++ src/app.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { EmailDto } from './emailDto';
+
+describe('AppService', () => {
+  let service: AppService;
+  let mailerService: MailerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AppService,
+        {
+          provide: MailerService,
+          useValue: jest.fn(),
+        },
+      ],
+    }).compile();
+
+    service = module.get<AppService>(AppService);
+    mailerService = module.get<MailerService>(MailerService);
+  });
+
+  it('should send email with correct details', async () => {
+    const emailDto: EmailDto = {
+      from: 'sender@email.com',
+      to: 'reciever@email.com',
+      subject: 'Testing',
+      text: 'BlaBlaBla',
+    };
+
+    const sendMailSpy = jest
+      .spyOn(mailerService, 'sendMail')
+      .mockResolvedValueOnce(null);
+
+    await service.sendLetter(emailDto);
+
+    expect(sendMailSpy).toHaveBeenCalledWith({
+      from: emailDto.from,
+      to: emailDto.to,
+      subject: emailDto.subject,
+      text: emailDto.text,
+    });
+
+    expect(sendMailSpy).toHaveBeenCalledTimes(1);
+  });
+});
